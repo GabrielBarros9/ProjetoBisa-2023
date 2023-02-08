5dd4ba37d04f2f60ccdc1038a184ac85e87216f3
GabrielBarros9 <49459539+GabrielBarros9@users.noreply.github.com>
2023-02-08 13:50:35 -0300

    Update dump.sql
    Atualizando Script

--- dump.sql
+++ dump.sql
@@ -1,27 +1,27 @@
 CREATE TABLE Tipo_Entrada (
-  id_tipo_entrada INT PRIMARY KEY AUTO_INCREMENT,
+  id_tipo_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(255)
 );
 
 CREATE TABLE Entrada (
-  id_entrada INT PRIMARY KEY AUTO_INCREMENT,
-  id_tipo_entrada INT NOT NULL,
+  id_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_tipo_entrada INT,
   descricao VARCHAR(255),
-  data_hora_entrada DATETIME NOT NULL,
-  valor_entrada float NOT NULL,
+  data_hora_entrada DATETIME,
+  valor_entrada DECIMAL(10,2)
   FOREIGN KEY (id_tipo_entrada) REFERENCES Tipo_Entrada (id_tipo_entrada)
 );
 
 CREATE TABLE Tipo_Saida (
-  id_tipo_saida INT PRIMARY KEY AUTO_INCREMENT,
+  id_tipo_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(255)
 );
 
 CREATE TABLE Saida (
-  id_saida INT PRIMARY KEY AUTO_INCREMENT,
-  id_tipo_saida INT NOT NULL,
+  id_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_tipo_saida INT,
   descricao VARCHAR(255),
-  data_hora_saida DATETIME NOT NULL,
-  valor_saida float NOT NULL,
+  data_hora_saida DATETIME,
+  valor_saida DECIMAL(10,2)
   FOREIGN KEY (id_tipo_saida) REFERENCES Tipo_Saida (id_tipo_saida)
 );
