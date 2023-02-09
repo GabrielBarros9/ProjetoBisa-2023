6eb4783487585b99fc1466561d15d0be3b2c6a1b
GabrielBarros9 <49459539+GabrielBarros9@users.noreply.github.com>
2023-02-09 11:19:31 -0300

    Update dump.sql
    Update

--- dump.sql
+++ dump.sql
@@ -5,10 +5,10 @@ CREATE TABLE Tipo_Entrada (
 
 CREATE TABLE Entrada (
   id_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_tipo_entrada INT,
+  id_tipo_entrada INT NOT NULL,
   descricao VARCHAR(255),
   data_hora_entrada DATETIME,
-  valor_entrada DECIMAL(10,2)
+  valor_entrada DECIMAL(10,2) NOT NULL
   FOREIGN KEY (id_tipo_entrada) REFERENCES Tipo_Entrada (id_tipo_entrada)
 );
 
@@ -19,9 +19,9 @@ CREATE TABLE Tipo_Saida (
 
 CREATE TABLE Saida (
   id_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_tipo_saida INT,
+  id_tipo_saida INT NOT NULL,
   descricao VARCHAR(255),
   data_hora_saida DATETIME,
-  valor_saida DECIMAL(10,2)
+  valor_saida DECIMAL(10,2) NOT NULL
   FOREIGN KEY (id_tipo_saida) REFERENCES Tipo_Saida (id_tipo_saida)
 );
