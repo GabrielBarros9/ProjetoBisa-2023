2bd4cdf6053fc5297abc34c9f8d4934c73930c86
gabrielbarros9 <gabrielbarrosteixeira9@gmail.com>
2023-02-23 21:34:24 -0300

    ProjetoBisa-MVC

--- dump.sql
+++ dump.sql
@@ -1,27 +1,41 @@
-CREATE TABLE Tipo_Entrada (
-  id_tipo_entrada INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255)
+CREATE DATABASE bisaweb;
+USE bisaweb;
+
+-- Criação da tabela Tipos_Entradas
+CREATE TABLE Tipos_Entradas (
+  id_tipo_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Entrada (
-  id_entrada INT PRIMARY KEY AUTO_INCREMENT,
+-- Criação da tabela Entradas
+CREATE TABLE Entradas (
+  id_entrada INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   id_tipo_entrada INT NOT NULL,
-  descricao VARCHAR(255),
-  data_hora_entrada DATETIME NOT NULL,
-  valor_entrada float NOT NULL,
-  FOREIGN KEY (id_tipo_entrada) REFERENCES Tipo_Entrada (id_tipo_entrada)
+  descricao VARCHAR(255) NOT NULL,
+  data_hora_entrada DATETIME,
+  valor_entrada DECIMAL(10,2) UNIQUE NOT NULL
+);
+	
+ALTER TABLE Entradas
+ADD CONSTRAINT fk_Entradas_Tipos_Entradas
+FOREIGN KEY (id_tipo_entrada) REFERENCES Tipos_Entradas(id_tipo_entrada);
+
+-- Criação da tabela Tipos_Saidas
+CREATE TABLE Tipos_Saidas (
+  id_tipo_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Tipo_Saida (
-  id_tipo_saida INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255)
-);
-
-CREATE TABLE Saida (
-  id_saida INT PRIMARY KEY AUTO_INCREMENT,
+-- Criação da tabela Saidas
+CREATE TABLE Saidas (
+  id_saida INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   id_tipo_saida INT NOT NULL,
-  descricao VARCHAR(255),
-  data_hora_saida DATETIME NOT NULL,
-  valor_saida float NOT NULL,
-  FOREIGN KEY (id_tipo_saida) REFERENCES Tipo_Saida (id_tipo_saida)
+  descricao VARCHAR(255)NOT NULL,
+  data_hora_saida DATETIME,
+  valor_saida DECIMAL(10,2) UNIQUE NOT NULL
 );
+
+ALTER TABLE Saidas
+ADD CONSTRAINT fk_Saidas_Tipos_Saidas
+FOREIGN KEY (id_tipo_saida) REFERENCES Tipos_Saidas(id_tipo_saida);
+
